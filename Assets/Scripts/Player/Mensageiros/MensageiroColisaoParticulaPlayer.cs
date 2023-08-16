8a1714a5ed370f3760722522a19e613393d3b252
Leonardo Marques <leomarques1905@gmail.com>
2023-08-16 20:06:16 -0300

    Correcao de colisoes da bala e das particulas de recuperacao de vida

--- Assets/Scripts/Player/Mensageiros/MensageiroColisaoParticulaPlayer.cs
+++ Assets/Scripts/Player/Mensageiros/MensageiroColisaoParticulaPlayer.cs
@@ -7,8 +7,24 @@ public class MensageiroColisaoParticulaPlayer : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     private ScriptPlayer sP;
+    private List<ParticleCollisionEvent> colisoes;
+    private int numeroDeColisoes;
+    private void Awake()
+    {
+        colisoes = new List<ParticleCollisionEvent>();
+    }
     void OnParticleCollision(GameObject other)
     {
-        sP.Curar(sP.GetVidaCuradaPorColisaoParticula);
+        ParticleSystem ps = other.GetComponent<ParticleSystem>();
+        if(ps.GetComponent<ParticleSystem>() != null)
+        {
+            numeroDeColisoes=ParticlePhysicsExtensions.GetCollisionEvents(ps,this.gameObject,colisoes);
+            for(int i=0;i<numeroDeColisoes;i++)
+            {
+                sP.Curar(sP.GetVidaCuradaPorColisaoParticula);
+            }
+            colisoes.Clear();
+        }
+        
     }
 }
